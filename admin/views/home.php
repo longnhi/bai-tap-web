4bc7a1c75fb6da3c3d399572060cfd60965a4efc
nguoicu <inuyashakagome9a3@gmail.com>
2021-12-31 00:54:25 +0700

    hoàn thành trang home admin

--- admin/views/home.php
+++ admin/views/home.php
@@ -3,7 +3,7 @@
             <div class="col-lg-6 mb-3 mt-3">
                 <div class="card h-100 shadow">
                     <div class="card-header text-center">
-                        <p>Có 0 áo đấu</p>
+                        <p>Có <?php echo count($jersey->all()); ?> áo đấu</p>
                     </div>
                     <div class="card-body d-flex align-items-center justify-content-center">
                         <a class="page-link" href="#">Quản lý áo đấu</a>
@@ -13,7 +13,7 @@
             <div class="col-lg-6 mb-3 mt-3">
                 <div class="card h-100 shadow">
                     <div class="card-header text-center">
-                        <p>Có 0 loại áo đấu</p>
+                        <p>Có <?php echo count($type->all()); ?> loại áo đấu</p>
                     </div>
                     <div class="card-body d-flex align-items-center justify-content-center">
                         <a class="page-link" href="#">Quản lý loại áo đấu</a>
@@ -23,7 +23,7 @@
             <div class="col-lg-6 mb-3 mt-3">
                 <div class="card h-100 shadow">
                     <div class="card-header text-center">
-                        <p>Có 0 câu lạc bộ</p>
+                        <p>Có <?php echo count($club->all()); ?> câu lạc bộ</p>
                     </div>
                     <div class="card-body d-flex align-items-center justify-content-center">
                         <a class="page-link" href="#">Quản lý câu lạc bộ</a>
@@ -33,7 +33,7 @@
             <div class="col-lg-6 mb-3 mt-3">
                 <div class="card h-100 shadow">
                     <div class="card-header text-center">
-                        <p>Có 0 tài khoản khách hàng</p>
+                        <p>Có <?php echo count($kh->all()); ?> tài khoản khách hàng</p>
                     </div>
                     <div class="card-body d-flex align-items-center justify-content-center">
                         <a class="page-link" href="#">Quản lý tài khoản</a>
